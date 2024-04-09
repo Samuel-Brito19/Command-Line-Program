d6d01520b73f8ef0203eecfc2c25807551b81bb2
Samuel-Brito19 <sambriton7@gmail.com>
2024-04-09 19:29:52 -0300

    Reading a file

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
-use std::env;
+use std::{env, fs};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    dbg!(args)
+
+    let query = &args[1];
+    let file_path = &args[2];
+
+    println!("Searching for {}", query);
+    println!("In file {}", file_path);
+
+    let contents = fs::read_to_string(file_path).expect("Should read the file");
+
+    print!("With text: \n{contents}");
+
+    dbg!(args);
 }
