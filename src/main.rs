0161ab79fd151b93d11ea6653d355c987e51aa49
Samuel-Brito19 <sambriton7@gmail.com>
2024-04-09 20:25:35 -0300

    Creating a struct config

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,24 @@ use std::{env, fs};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    let config = parse_config(&args);
 
-    let query = &args[1];
-    let file_path = &args[2];
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
 
-    println!("Searching for {}", query);
-    println!("In file {}", file_path);
+    let contents = fs::read_to_string(config.file_path).expect("Should read the file");
 
-    let contents = fs::read_to_string(file_path).expect("Should read the file");
+    println!("With text: \n{contents}");
+}
 
-    print!("With text: \n{contents}");
+struct Config {
+    query: String,
+    file_path: String,
+}
 
-    dbg!(args);
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let file_path = args[2].clone();
+
+    Config { query, file_path }
 }
