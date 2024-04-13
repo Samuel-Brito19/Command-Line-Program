b429a09da0a7235257a47ec720ef00952b3fb3ba
Samuel-Brito19 <sambriton7@gmail.com>
2024-04-13 10:44:21 -0300

    Finishing the project

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,13 @@ use minigrep::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.file_path);
-
     // não precisa unrawap pois retornará () em caso de sucesso
     if let Err(e) = minigrep::run(config) {
-        print!("Aplication error: {e}");
+        eprintln!("Aplication error: {e}");
         process::exit(1);
     }
 }
